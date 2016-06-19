5686def2374e54c3f3684ab61fc589e44a03579f
Ricardo Clarke <wolfpack3371@gmail.com>
2016-06-18 20:35:12 -0400

    nested my routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :beats
-  resources :producers
+  resources :producers do
+    resources :beats
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
